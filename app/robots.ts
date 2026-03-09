93c41e9a77072bedb407e3666ad8da11b78b6f39
Abhishek Singh <darvince.1561@gmail.com>
2026-03-09 18:54:52 +0530

    fixed indexing issue

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: '*',
             allow: '/',
-            disallow: ['/dashboard/', '/api/'], // Disallow dashboard and API routes
+            disallow: ['/dashboard', '/api', '/login', '/signup', '/pending', '/admin'],
         },
         sitemap: process.env.NEXT_PUBLIC_APP_URL ? `${process.env.NEXT_PUBLIC_APP_URL}/sitemap.xml` : 'https://www.medikarya.in/sitemap.xml',
     }
